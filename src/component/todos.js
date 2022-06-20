50c91546af46718fba930cae9e541af9e9f2a235
dennis wang <dwang984@usc.edu>
2022-06-20 02:42:38 -0400

    finished lab2

--- src/component/todos.js
+++ src/component/todos.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "../component/todos.css";
 import { Card, CardContent, Grid } from "@mui/material";
-
+import { ListItemButton, ListItemText, Checkbox } from "@mui/material";
+import { color } from "@mui/system";
+import deleteTodo from "../pages/Home.js"
+import Home from "../pages/Home";
+ 
 // 1. This component formats and returns the list of todos.
 // 2. Treat the question mark like an if statement.
 // If the todos array has items in the list [todos.length], we want to return the list
@@ -9,17 +13,20 @@ import { Card, CardContent, Grid } from "@mui/material";
 // 3. The map function is called to assign each array item with a key
 // 4. Think of lines 14-23 as a loop. For each todo in the todo list, we want to give the list item
 // a key, and it's own card shown in the UI
-const Todos = ({ todos }) => {
+const Todos = ({ todos, deleteTodo}) => {
   const todoList = todos.length ? (
     todos.map((todo) => {
       return (
         <Grid key={todo.id}>
-          <Card>
+          <Card style={{marginTop:10}}>
             {/* Remember, we set the local state of this todo item when the user submits the form in 
             AddTodo.js. All we need to do is return the todo list item {todo.content} */}
-            <CardContent>
-              <span style={{ padding: "50px" }}>{todo.content}</span>
-            </CardContent>
+            <ListItemButton component = "a" href = "#simple-list">
+              <Checkbox onClick ={() => deleteTodo(todo.id)} color = "primary" style = {{paddingLeft:10}}>
+                
+              </Checkbox>
+              <ListItemText primary = {todo.content} secondary = {todo.date}></ListItemText>
+            </ListItemButton>
           </Card>
         </Grid>
       );
@@ -34,5 +41,6 @@ const Todos = ({ todos }) => {
     </div>
   );
 };
+
 
 export default Todos;
